2dad411d6b945ab012235cef0ec9755662bbe235
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-11-23 21:15:54 +0900

    Update getDevicesList.py

--- app/src/getDevicesList.py
+++ app/src/getDevicesList.py
@@ -1,47 +1,44 @@
 import os
 import time
+import json
 import hashlib
 import hmac
 import base64
-import json
 import uuid
 import requests
 import json
+import datetime
+from os.path import exists
 
-# Create api header
-def create_api_header(token:str, secret:str) -> dict:
-    nonce = str(uuid.uuid4())
-    t = int(round(time.time() * 1000))
-    string_to_sign = '{}{}{}'.format(token, t, nonce)
+dir_name = "../responses"
+if not exists(dir_name):
+    os.makedirs(dir_name)
 
-    string_to_sign = bytes(string_to_sign, 'utf-8')
-    secret = bytes(secret, 'utf-8')
+token = os.environ["SWITCHBOT_TOKEN"]
+secret = os.environ["SWITCHBOT_SECRET"]
+nonce = str(uuid.uuid4())
+t = int(round(time.time() * 1000))
+string_to_sign = "{}{}{}".format(token, t, nonce)
+string_to_sign = bytes(string_to_sign, "utf-8")
+secret = bytes(secret, "utf-8")
+sign = base64.b64encode(
+    hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest()
+)
 
-    sign = base64.b64encode(
-        hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
+apiHeader = {}
+apiHeader["Authorization"] = token
+apiHeader["Content-Type"] = "application/json"
+apiHeader["charset"] = "utf8"
+apiHeader["t"] = str(t)
+apiHeader["sign"] = str(sign, "utf-8")
+apiHeader["nonce"] = nonce
 
-    apiHeader = {}
-    apiHeader["Authorization"] = token
-    apiHeader['Content-Type']='application/json'
-    apiHeader['charset']='utf8'
-    apiHeader["t"] = str(t)
-    apiHeader["sign"] = str(sign, 'utf-8')
-    apiHeader["nonce"] = nonce
-
-    return apiHeader
-
-
-token = os.environ['SWITCHBOT_TOKEN']
-secret = os.environ['SWITCHBOT_SECRET']
-
-apiHeader = create_api_header(token, secret)
-
-response = requests.get(
-    "https://api.switch-bot.com/v1.1/devices", headers=apiHeader)
+response = requests.get("https://api.switch-bot.com/v1.1/devices", headers=apiHeader)
 devices = response.json()
 
-# output json file
-with open('../docs/devicesList.json', 'w', encoding='utf-8') as f:
-    json.dump(devices, f, ensure_ascii=False)
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+response_file = f"{dir_name}/devices_{timestamp}.json"
+with open(response_file, "w") as f:
+    json.dump(devices, f)
 
-print("Success get device list.")
+print("Success get devices list.")
